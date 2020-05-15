a0b8859f180db2e50f5a4b42368f173a531b9fe8
BhupeshVerma <bhupesh.v94@gmail.com>
2020-05-15 09:19:52 +0530

    Added new google fonts

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <WelcomeMessage />
     </div>
   );
 }
---
+++ src/app/WelcomeMessage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const WelcomeMessage= () => {
+    return <h1>Welcome to Cryptodash</h1>
+}
+
+export default WelcomeMessage;
