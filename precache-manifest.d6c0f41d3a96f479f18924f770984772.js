0aaa6abfb0cb055b005904f19dec8b3816bce977
BhupeshVerma <bhupesh.v94@gmail.com>
2020-05-31 23:13:56 +0530

    Updates

---
+++ precache-manifest.d6c0f41d3a96f479f18924f770984772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f733656482a224f6ea526ae601bdeb",
+    "url": "/cryptodash/index.html"
+  },
+  {
+    "revision": "e1d11d46aaa39c603799",
+    "url": "/cryptodash/static/css/main.51bd47cd.chunk.css"
+  },
+  {
+    "revision": "3b7e65e352e5b123b1e9",
+    "url": "/cryptodash/static/js/2.86a70e89.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/cryptodash/static/js/2.86a70e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d11d46aaa39c603799",
+    "url": "/cryptodash/static/js/main.4390dd88.chunk.js"
+  },
+  {
+    "revision": "075f8769346112e49dc6",
+    "url": "/cryptodash/static/js/runtime-main.d15104a9.js"
+  }
+]);
